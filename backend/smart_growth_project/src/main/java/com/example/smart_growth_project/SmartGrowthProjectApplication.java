8471beefc3b657afb472e7fc0d5d3bf615f13471
Your Name <yours.suren2021@emil.com>
2026-05-16 11:18:27 +0530

    Initail project setup

---
+++ backend/smart_growth_project/src/main/java/com/example/smart_growth_project/SmartGrowthProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_growth_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGrowthProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGrowthProjectApplication.class, args);
+	}
+
+}
---
+++ backend/smart_growth_project/src/test/java/com/example/smart_growth_project/SmartGrowthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_growth_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGrowthProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
